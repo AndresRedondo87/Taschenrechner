917f3e42cdd39c48628f385c0bb32f0b1c92c4d8
AndresRedondo87 <66962177+AndresRedondo87@users.noreply.github.com>
2020-06-24 17:54:49 +0200

    Kommentare Info (+summary für Dokumentation. UserStories nochmal added?

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -4,12 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Taschenrechner
+
+namespace Taschenrechner_Iteration_1
 {
+    /// <summary>
+    /// Taschenrechner: zwei Zahlen eingeben, um deren Summe berechnen zu lassen
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
+            //Kommentary infos: dreimal slash und  summary in xml, wird als info für später verwenden
+            // es wird für das nachfolgende Konstrukt unten Dokumentation verwendet. 
+
+            //Kommentare sind generell super hilfreich für die weiterentwicklung. 
+            //Kann man sogar web adressen hinzugefügt werden.
+
+            //------------------------------------------------------------------
+            //USER STORIES Taschenrechner Anforderungen
+            //------------------------------------------ -
+            //Erste Beispiel(2x Stories) -Iteration #1:	
+            //Titel: Addieren
+            //Story:Als Benutzer möchte ich zwei Zahlen eingeben, um deren Summe berechnen zu lassen
+            //Akzeptanzkriterien:
+            //			-Zahlen zwischen 0 und 10 können addiert werden.
+            //			-[erweiterung als Beispiel: Zahlen in HEX eingeben können]
+
+            //Titel: Starten
+            //Story: Als Benutzer möchte ich den Taschenrechner schnell aufrufen können, um mein Resultat schnell zu bekommen
+            //Akzeptanzkriterien:
+            //			-Die Anwendung wird innerhalb von 2 Sekunden auf einem Rechner gestartet
+            //			- Die Anwendung läuft auf einem Rechner mit Windows 10
+            //------------------------------------------------------------------
+
+               
+
         }
     }
 }
