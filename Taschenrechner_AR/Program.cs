eaa2f52c3b78abdde5364388e6b881437107a759
AndresRedondo87 <66962177+AndresRedondo87@users.noreply.github.com>
2020-08-22 19:31:13 +0200

    Video 52 Erstellen einer Methode - Wir verlangen die Eingabe (erster summand) Methode noch nicht verwendet

--- Taschenrechner_AR/Program.cs
+++ Taschenrechner_AR/Program.cs
@@ -254,5 +254,81 @@ namespace Taschenrechner_AR
         }
 
         //Video 51 - Versionsverwaltung nicht vergesen, Immer commit und push!!
+
+
+        /// <summary>
+        /// Methoden Definieren in 7 Schritten
+        /// -(Optional)Modifizierer definieren (Zugriffsmodifizierer)
+        /// -Datentyp des Rueckgabewertes definieren
+        /// -Methodennamen definieren
+        /// -Rundeklammern an den Methodennamen anfügen
+        /// -Ueberlegen welche Parameter benoetigt werden (Optional diese zu definieren)
+        /// -Geschweifte Klammern einfuegen
+        /// -Methode implementieren (Anweisungen in den Methodenrumpf schreiben)
+        /// </summary>
+
+        /// Video 52 Erstellen einer Methode - Wir verlangen die Eingabe (erster summand)
+        /// Merken return ist immer das letzte in einer Methode. Was danach kommt, wird nie ausgefuehrt.
+        /// Methoden können oft verbessert werden: von HoleErstenSumanden erst kann man den ausgabeText als Parameter eingeben, so ist es viel flexibler.
+        /// Dann kann es auch zu HoleSumanden werden um nich nur fuers erste sondern fuer viele summanden.
+
+        static string HoleSumanden(string ausgabeText)
+        {
+            Console.Write(ausgabeText);
+            string Summand = Console.ReadLine();
+
+            return Summand;
+        }
+
+
+//BEISPIEL TEXT BZW HAUPT INFOS
+/*namespace Methoden
+{
+    /// <summary>
+    /// Zeigt dir die wichtigsten Grundlagen (insbesondere Begriffe) rund um das
+    /// Thema Methoden. Außerdem werden von hier alle Beispiele zum Thema Methoden
+    /// aufgerufen.
+    /// </summary>
+    class Einfuehrung
+    {
+        // Bei der Definition einer Methode (auch Methodendefinition genannt) geht es darum,
+        // das du den Quellcode schreibst der sagt was zutun ist, wenn diese Methode ausgeführt
+        // wird. Im folgenden ein kurzer Überblick über die Bestandteile einer Methode:
+
+        // ZUGRIFFSMODIFIZIERER (access modifier): Definiert von wo die Methode erreichbar ist.
+        // Häufig verwendet sind private - Methode kann nur innerhalb der Klasse verwendet werden
+        // und public - Methode kann von jeglicher Klasse verwendet werden.
+        // Mögliche Werte sind: public, private, protected und internal für Details siehe MSDN
+        // https://msdn.microsoft.com/de-de/library/ms173121.aspx
+
+        // PARAMETER: Jeder Methode kann optional ein oder mehrere Parameter übergeben werden.
+        // Jeder Parameter besteht aus einem Datentypen und einem Namen. Wenn eine Methode mehrere
+        // Parameter hat, dann werden diese mit Komma getrennt.
+
+        // Optional können zwischen Zugrifssmodifizierer und Datentyp des Rückgabewerts noch die
+        // Schlüsselwörter STATIC und ASYNC verwendet werden. Dies wird in separaten Projekten
+        // besprochen (z.B. SchlüsselwortStatic)
+
+        // Aufbau:
+        // Zugriffsmodifizierer (engl.: access modifier or method visibility)
+        //  |
+        //  |   Datentyp des Rückgabewerts (engl.: datatype of return value)
+        //  |    |
+        //  |    |               Methodenname in CamelCase-Notation
+        //  |    |                   |
+        //  |    |                   |                            Datentyp vom ersten Parameter
+        //  |    |                   |                             |
+        //  |    |                   |                             |      Name des ersten Parameters
+        //  |    |                   |                             |        |
+        public void MethodeOhneRueckgabewertAberMitEinemParameter(int parameterName) // <- Methodenkopf
+        {
+            // Dies ist der Methodenrumpf. Er beginnt mit einer geöffneten geschweiften Klammer
+            // und muss mit einer geschlossenen geschweiften Klammer beendet werden.
+            throw new NotImplementedException("Methode ist nur als Beispiel gedacht!");
+
+            // Nur wenn die Methode keinen Wert zurück gibt (also der Datentyp des Rückgabewertes
+            // void ist), kann auf das "return" am Ende der Methode verzichtet werden.
+        }
+*/
     }
 }
